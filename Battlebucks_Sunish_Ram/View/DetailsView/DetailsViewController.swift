f2fe0148968f839d52eeb00e85c9ddc524b94407
Sunish1993 <32665361+Sunish1993@users.noreply.github.com>
2024-09-28 17:22:14 +0530

    Update DetailsViewController.swift

--- Battlebucks_Sunish_Ram/View/DetailsView/DetailsViewController.swift
+++ Battlebucks_Sunish_Ram/View/DetailsView/DetailsViewController.swift
@@ -7,23 +7,16 @@
 
 import UIKit
 
-class DetailsViewController: UIViewController {
+class DetailsViewController: BaseViewController {
 
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var detailImageView: LazyImageView!
+    var detailsViewModel = DetailsViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        self.showImageDetails()
+     }
+   
 
 }
